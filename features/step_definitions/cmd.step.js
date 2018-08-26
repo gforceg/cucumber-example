2737e395ef42c1842758f08d4483b890f28cf796
Greg Hedin <greg.hedin@gmail.com>
2018-08-26 15:38:36 -0600

    refactor(cucumber steps) make commands and filenames variable

--- features/step_definitions/cmd.step.js
+++ features/step_definitions/cmd.step.js
@@ -2,7 +2,7 @@ import * as childProcess from 'child_process'
 import * as path from 'path'
 import { When } from 'cucumber'
 
-When(/^I run: (json-to-csv.js) (users.json)$/, function (scriptName, fileName) {
+When(/^I run: ([\w-.]+) ([\w-.]+)$/, function (scriptName, fileName) {
   const filePath = path.join(this.filesRoot, fileName)
   childProcess.spawnSync('node', [scriptName, filePath, this.filesRoot])
 })
--- features/step_definitions/fs.steps.js
+++ features/step_definitions/fs.steps.js
@@ -2,12 +2,12 @@ import * as fs from 'fs'
 import * as assert from 'assert'
 import { Given, Then } from 'cucumber'
 
-Given(/^the file (users.json) contains$/, function (fileName, docString) {
+Given(/^the file ([\w-.]+) contains$/, function (fileName, docString) {
   // create / overwrite the file
   fs.writeFileSync(this.getFilePath(fileName), docString)
 })
 
-Then(/^the file (users.csv) should contain$/, function (fileName, docString) {
+Then(/^the file ([\w-.]+) should contain$/, function (fileName, docString) {
   // 1. read the file and confirm that it includes the string
   // 2. assert that the file contains the correct csv
 
